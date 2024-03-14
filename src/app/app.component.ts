a32c83a38ab43733597a2d129483bd95631132cb
Souradeep Sarkar <88938453+SouradeepSarkar77@users.noreply.github.com>
2024-03-14 15:40:19 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { ngxCsv } from 'ngx-csv/ngx-csv';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Download-Excel-Two';
+
+  data = [
+    {
+      name: "Test 1",
+      age: 13,
+      average: 8.2,
+      approved: true,
+      description: "using 'Content here, content here' "
+    },
+    {
+      name: 'Test 2',
+      age: 11,
+      average: 8.2,
+      approved: true,
+      description: "using 'Content here, content here' "
+    },
+    {
+      name: 'Test 4',
+      age: 10,
+      average: 8.2,
+      approved: true,
+      description: "using 'Content here, content here' "
+    },
+  ];
+
+  fileDownload(){
+    var options = {
+      fieldSeparator: ',',
+      quoteStrings: '"',
+      decimalseparator: '.',
+      showLabels: true,
+      showTitle: true,
+      title: 'Report Data',
+      useBom: true,
+      headers: ["Name", "Age", "Average", "Approved", "Description"]
+    };
+
+    new ngxCsv(this.data, "DownloadReport", options);
+  }
+}
